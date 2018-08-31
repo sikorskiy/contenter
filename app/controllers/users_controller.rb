e34ac44aca6c11f549cee96c3897907c16c82804
Adiutant <lagynkov.rom@gmail.com>
2018-08-31 12:47:34 +0300

    added_show_to_users_added_statistics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def show
+    @f = current_user
   end
 
   def index
     @u = User.all
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :camps
   resources :companies
+  get  "/camp/statistic", to: "camps#statistic"
   get  "/camp/check", to: "camps#check"
   get  "/users/index", to: "users#index"
+  get  "/users/show", to: "users#show"
   devise_for :users
   root to: 'camps#index'
   post 'camps/autosave', to: 'camps#autosave'
