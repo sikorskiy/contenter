ea15d806f2e2181e15258874ebbb7060324e9d86
Vasiliy Sikorskiy <vasiliy.sikorskiy@gmail.com>
2018-09-19 21:47:19 +0300

    badges

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :camps
   resources :companies
   resources :badges
-  resources :users
   devise_for :users
+  resources :users
   root to: 'camps#index'
   post 'camps/autosave', to: 'camps#autosave'
   get '/camp/statistics', to: 'camps#statistics'
